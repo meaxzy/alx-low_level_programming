abb10590d7760a335ca37bb6ee35117abeade580
meaxzy <olayinkawu@gmail.com>
2023-06-15 15:02:51 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main- Use the function printf to output
+ * Return: 0 always
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+}
