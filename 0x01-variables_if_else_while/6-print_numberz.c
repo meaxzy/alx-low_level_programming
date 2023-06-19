000d59f75e4f092a32cdb8d569887c7279679191
meaxzy <olayinkawu@gmail.com>
2023-06-19 14:51:52 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - A program that prints all single digit numbers of base 10
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
