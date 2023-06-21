948f62b18cd2890e05a2c3777ebf1fdb92c8b6e3
meaxzy <olayinkawu@gmail.com>
2023-06-21 14:16:46 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ * @n: input number as an integer
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int m;
+
+	m = n % 10;
+	if (m < 0)
+	{
+		_putchar(-m + 48);
+		return (-m);
+	}
+	else
+	{
+		_putchar(m + 48);
+		return (m);
+	}
+}
