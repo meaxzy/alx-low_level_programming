357c0c343f03dab16daec945f07825b98a4363e5
meaxzy <olayinkawu@gmail.com>
2023-06-23 07:58:23 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,12 +12,15 @@ void print_square(int size)
 	}
 	else
 	{
-		int i;
+		int i, j;
 
-		for (i = 0; i <= size; i++)
+		for (i = 0; i < size; i++)
 		{
-			_putchar('#');
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
