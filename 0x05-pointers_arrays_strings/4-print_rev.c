d17ae66069a1d64b57c01c31fac27d41b15a3d87
meaxzy <olayinkawu@gmail.com>
2023-06-28 16:53:58 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,18 +7,18 @@
 void print_rev(char *s)
 {
 
-	int longi = 0;
-	int i;
+int longi = 0;
+int i;
 
-	while (*s != '\0')
-	{
-		longi++;
-		s++;
-	}
-	s--;
-	for (i = longi; i > 0; i--)
-	{
-		_putchar(*s);
-	}
+while (*s != '\0')
+{
+longi++;
+s++;
+}
+s--;
+for (i = longi; i > 0; i--)
+{
+_putchar(*s);
+}
 }
 
