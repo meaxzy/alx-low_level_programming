af8612cd54e5e41236eecc270ab62913b23e3378
meaxzy <olayinkawu@gmail.com>
2023-07-09 16:41:50 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,16 +13,17 @@ int main(int argc, char *argv[])
 	int i, j, add = 0;
 
 	for (i = 0; i < argc; i++)
+	{
 		for (j = 0; argv[i][j] != '\0'; j++)
+		{
 			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
 				return (1);
 			}
-			else
-			{
-				add += atoi(argv[i]);
-			}
+		}
+		add += atoi(argv[i]);
+	}
 	printf("%d\n", add);
 	return (0);
 }
