a7c99ac0ee5dfbfb09179dc5c70e71d68b80865d
meaxzy <olayinkawu@gmail.com>
2023-07-11 10:27:32 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -22,19 +22,19 @@ int main(int argc, char **argv)
 	while (cents > 0)
 	{
 		if (cents >= 25)
-			cents += 25;
+			cents -= 25;
 
 		if (cents >= 10)
-			cents += 10;
+			cents -= 10;
 
 		if (cents >= 5)
-			cents += 5;
+			cents -= 5;
 
 		if (cents >= 2)
-			cents += 2;
+			cents -= 2;
 
 		if (cents >= 1)
-			cents += 1;
+			cents -= 1;
 		i += 1;
 	}
 	printf("%d\n", i);
