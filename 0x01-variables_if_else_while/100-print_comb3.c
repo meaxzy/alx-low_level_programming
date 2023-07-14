aa8e0186d5f23e75fc54ecc750a009b05c6b34a8
meaxzy <olayinkawu@gmail.com>
2023-07-14 14:03:25 -0700

    new task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - program that prints all possible different combinations of 2 digits
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+		for (j = 0; j <= '9'; j++)
+		{
+			_putchar([i]);
+			_putchar([j]);
+		}
+			if (i != '8' || (i == '8' && j != '9'))
+			{
+				_putchar(' ');
+				_putchar(',');
+			}
+			_putchar('\n');
+}
+
