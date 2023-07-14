5f2cf67603e1ee540367a8c1415b41e02cc1009b
meaxzy <olayinkawu@gmail.com>
2023-07-14 14:14:20 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,8 +8,8 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 9; i++)
-		for (j = 0; j <= '9'; j++)
+	for (i = '0'; i <= '9'; i++)
+		for (j = '0'; j <= '9'; j++)
 		{
 			_putchar([i]);
 			_putchar([j]);
