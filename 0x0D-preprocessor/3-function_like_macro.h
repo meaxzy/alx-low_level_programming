0ba548de8f2c30f84532199d3245a6dce2729856
meaxzy <olayinkawu@gmail.com>
2023-07-18 08:37:05 -0700

    task 3

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifdef MACROLIKE_H
+#define MACROLIKE_H
+/**
+ * File: 3-function_like_macro.h
+ *
+ * @x: Input
+ * Description: Header file that computes absolute value
+ */
+#define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
+
+#endif
