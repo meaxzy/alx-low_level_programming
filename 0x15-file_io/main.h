b892f80ef3b170e56bc441ee4a0f76bbaca23809
meaxzy <olayinkawu@gmail.com>
2023-09-04 11:24:36 -0700

    Readme

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,16 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include<sys/types.h>
-#include<sys/stat.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
 
-Ssize_t read_textfile(const char *filename, size_t letters);
-Int _putchar(char c);
-Int create_file(const char *filename, char *text_content);
-Int append_text_to_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
