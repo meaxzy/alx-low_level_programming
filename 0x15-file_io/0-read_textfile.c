b3287fcbf1e1cc8da1ef6c99ac752c99ffe69eeb
meaxzy <olayinkawu@gmail.com>
2023-09-04 11:19:40 -0700

    tasks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,43 +1,28 @@
-#include “main.h”
+#include "main.h"
+#include <stdlib.h>
 
 /**
- * read_textfile – reads a text file and prints
- *  it to the POSIX standard output.
- * @filename: pointer to file containing chars.
- * @letters: letters to be read and printed.
- *
- * Return: number of letters it could read & print
- *         otherwise 0.
+ * read_textfile- Read text file print to output stream.
+ * @filename: text file with descriptor
+ * @letters: number of letters to be read
+ * Return: the number of bytes read and printed
+ * 0 when function fails or filename is NULL.
  */
-
-Ssize_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	Ssize_t  op, r, wr;
-	Char *storage;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	If (filename == NULL)
-		Return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-	Storage = malloc(sizeof(char) * letters);
-	/* allocating memory */
-
-	If (storage == NULL)
-		Return (0);
-	/* checking if malloc was succesful */
-
-	Op = open(filename, O_RDONLY); /* opens file in read only mode*/
-	R = read(op, storage, letters);
-	Wr = write(STDOUT_FILENO, storage, r);
-
-	If (op == -1 || r == -1 || wr == -1 || wr != r)
-	{
-		Free(storage);
-		Return (0);
-	}
-
-	Free(storage);
-	Close(op);
-
-	Return (wr);
-	/* returning bytes stored in wr.*/
+	free(buf);
+	close(fd);
+	return (w);
 }
