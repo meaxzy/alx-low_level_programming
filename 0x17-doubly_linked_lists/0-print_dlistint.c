23d919a32a8306b67fbbbbd22f3d04ce06d5db6f
meaxzy <olayinkawu@gmail.com>
2023-10-12 08:52:16 -0700

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -4,7 +4,7 @@
  * print_dlistint - a function that print the list
  *
  * @h: pointer to the head
- * return: number of the nodes
+ * Return: number of the nodes
  */
 
 size_t print_dlistint(const dlistint_t *h)
@@ -18,5 +18,5 @@ size_t print_dlistint(const dlistint_t *h)
 		count++;
 	}
 	
-	return count;
+	return (count);
 }
