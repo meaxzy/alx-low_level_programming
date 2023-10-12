1f7e3642abe43283d878e460e12f96e081508b11
meaxzy <olayinkawu@gmail.com>
2023-10-12 12:33:23 -0700

    task 3

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -20,17 +20,16 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new_node == NULL)
 	{
 		return (NULL);
-
-		new_node->n = n;
-		new_node->prev = NULL;
-		new_node->next = *head;
-
-		if (*head != NULL)
-		{
-			(*head)->prev = new_node;
-		}
+	}
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+	
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
 	}
 	*head = new_node;
 
-	return (new_node);
+	return (*head);
 }
