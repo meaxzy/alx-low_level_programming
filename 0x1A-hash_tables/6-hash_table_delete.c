6378e2c2c4bf6d6ae65c71d5cccb58eb89d2eb7c
meaxzy <olayinkawu@gmail.com>
2023-11-04 16:29:14 -0700

    add task

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,24 +8,24 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int index;
-    hash_node_t *current, *temp;
+	unsigned long int index;
+	hash_node_t *current, *temp;
 
-    if (ht == NULL)
-        return;
+	if (ht == NULL)
+		return;
 
-    for (index = 0; index < ht->size; index++)
-    {
-        current = ht->array[index];
-        while (current != NULL)
-        {
-            temp = current;
-            current = current->next;
-            free(temp->key);
-            free(temp->value);
-            free(temp);
-        }
-    }
-    free(ht->array);
-    free(ht);
+	for (index = 0; index < ht->size; index++)
+	{
+		current = ht->array[index];
+		while (current != NULL)
+		{
+			temp = current;
+			current = current->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
