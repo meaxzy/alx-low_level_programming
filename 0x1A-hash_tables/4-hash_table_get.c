0cfa14c102410e6cabe8744d0e147877bb1517e9
meaxzy <olayinkawu@gmail.com>
2023-11-04 15:26:12 -0700

    add task

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -4,29 +4,29 @@
  * hash_table_get - Retrieves the value associated with a key in a hash table.
  * @ht: The hash table to search in.
  * @key: The key to search for.
- * 
+ *
  * Return: The value associated with the key, or NULL if key couldn't be found.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
 	hash_node_t *current;
-	
+
 	if (ht == NULL || key == NULL || key[0] == '\0')
 		return (NULL);
-	
+
 	index = hash_djb2((const unsigned char *)key) % ht->size;
-	
+
 	current = ht->array[index];
-	
+
 	while (current != NULL)
 	{
 		if (strcmp(current->key, key) == 0)
 		{
-			return current->value;
+			return (current->value);
 		}
 		current = current->next;
 	}
-	
+
 	return (NULL);
 }
